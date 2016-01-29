582ed21129d2543cdd1e03d309cda4d0a3d83f64
whiteada <whiteada@gmail.com>
2016-01-29 20:35:31 +0100

    Dotween, kontent do testowej sceny testowego levelu :D

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnetr2D(Collider2D col)
+    {
+        if ( col.gameObject.layer == LayerMask.NameToLayer("hero") )
+        {
+            Debug.Log("Jestem w środku");
+        }
+    }
+}
---
+++ Assets/Scripts/HeroTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class HeroTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.DOMoveX(transform.position.x+100,5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

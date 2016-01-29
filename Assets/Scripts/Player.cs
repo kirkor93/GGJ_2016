44ea8b3dcdd351dc747e0a5c23e6a90d063ee391
Krzysztof Szczech <krzysztof.szczech@hotmail.com>
2016-01-29 21:31:05 +0100

    Nowa wersja inputu

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,9 +4,42 @@ namespace Assets.Scripts
 {
     public abstract class Player : MonoBehaviour
     {
-        public abstract void OnMove(Vector2 direction);
-        public abstract void OnJumpStart();
-        public abstract void OnJumpRelease();
+        [Range(0.0f, 100.0f)]
+        public float Acceleration;
+        [Range(0.0f, 1000.0f)]
+        public float MaxMovementSpeed;
+
+        public Collider2D GroundTestCollider;
+
+        private Rigidbody2D _rigidbody;
+        private float _currentMovementSpeed;
+        private bool _flying;
+
+        protected virtual void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        public virtual void OnMove(Vector2 direction)
+        {
+            float targetMovementSpeed = _currentMovementSpeed + Acceleration * direction.x;
+            _currentMovementSpeed = Mathf.Lerp(_currentMovementSpeed, targetMovementSpeed, 0.5f);
+
+            _rigidbody.velocity = Vector2.right * _currentMovementSpeed * Time.deltaTime;
+        }
+
+        public virtual void OnJumpStart()
+        {
+            if (_flying)
+            {
+                return;
+            }
+        }
+
+        public virtual void OnJumpRelease()
+        {
+            
+        }
         public abstract void OnActionStart();
         public abstract void OnActionRelease();
     }

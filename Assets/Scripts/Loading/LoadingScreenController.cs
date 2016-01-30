fc173aa5281f28b009e3ee45f499d4e83ea83816
Krzysztof Szczech <krzysztof.szczech@hotmail.com>
2016-01-30 17:05:09 +0100

    Szablon main menu, loading screen

--- Assets/Scripts/Loading/LoadingScreenController.cs
+++ Assets/Scripts/Loading/LoadingScreenController.cs
@@ -1,10 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Assets.Scripts;
+using UnityEngine.UI;
 
 public class LoadingScreenController : MonoBehaviour
 {
     public Player[] Characters;
+    public Text LoadingText;
+    public float TextChangeInterval = 1.0f;
+
+    public string[] LoadingTexts = {"Loading.", "Loading..", "Loading..."};
+
+    protected void Start()
+    {
+        StartCoroutine(AnimateText());
+    }
+
+    private IEnumerator AnimateText()
+    {
+        while (true)
+        {
+            foreach (string loadingText in LoadingTexts)
+            {
+                LoadingText.text = loadingText;
+                yield return new WaitForSeconds(TextChangeInterval);
+            }
+        }
+    }
 
     protected void Update()
     {

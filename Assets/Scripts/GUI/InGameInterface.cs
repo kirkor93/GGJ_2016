457263171623f4db92c6dade5ee132abb2a4282a
Krzysztof Szczech <krzysztof.szczech@hotmail.com>
2016-01-30 14:12:44 +0100

    HUD w grze

---
+++ Assets/Scripts/GUI/InGameInterface.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.GUI
+{
+    public class InGameInterface : MonoBehaviour
+    {
+        public Goat Goat;
+        public Chicken Chicken;
+
+        public Image GoatHealthBar;
+        public Image ChickenHealthBar;
+        public Text GoatScore;
+        public Text ChickenScore;
+
+        protected void Update()
+        {
+            GoatScore.text = string.Format("Score: {0}", Goat.CurrentScore);
+            ChickenScore.text = string.Format("Score: {0}", Chicken.CurrentScore);
+
+            //todo: bar fill
+        }
+    }
+}

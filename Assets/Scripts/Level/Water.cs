fcfd88a4c69d7794131bbe90b1ac531e2e9a608f
whiteada <whiteada@gmail.com>
2016-01-30 16:14:30 +0100

    Woda i poprawka do trampoliny (colllider).

---
+++ Assets/Scripts/Level/Water.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class Water : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.layer == LayerMask.NameToLayer("hero"))
+        {
+           // if ( col.gameObject.name.Contains("Goat") )
+                //col.gameObject.GetComponent<Animation>().cl
+
+            if (col.transform.localScale.x > 0)
+                col.transform.DOMoveX(col.transform.position.x + col.gameObject.GetComponent<SpriteRenderer>().bounds.size.x * 3, 2);
+            if (col.transform.localScale.x < 0)
+                col.transform.DOMoveX(col.transform.position.x - col.gameObject.GetComponent<SpriteRenderer>().bounds.size.x * 3, 2);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.layer == LayerMask.NameToLayer("hero"))
+        {
+            col.transform.DOKill();
+        }
+    }
+}

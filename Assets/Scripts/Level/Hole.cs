226eb2d6b3ec987d5e37269021aed4b0c4e6a0e1
whiteada <whiteada@gmail.com>
2016-01-30 17:07:00 +0100

    Dziura

---
+++ Assets/Scripts/Level/Hole.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts;
+
+public class Hole : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.layer == LayerMask.NameToLayer("hero"))
+        {
+            col.gameObject.GetComponent<Player>().HitPoints = 0;
+        }
+    }
+}

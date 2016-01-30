fb9efce64afc1e26f9391431f29a814642bfd87b
Krzysztof Szczech <krzysztof.szczech@hotmail.com>
2016-01-30 22:37:29 +0100

    Main menu

--- Assets/Scripts/GUI/MainMenu.cs
+++ Assets/Scripts/GUI/MainMenu.cs
@@ -1,11 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using DG.Tweening;
 
 public class MainMenu : MonoBehaviour
 {
     public RectTransform MainMenuPanel;
     public RectTransform CreditsPanel;
+    public RectTransform GameName;
+    public RectTransform TargetNamePos;
 
+    protected void Awake()
+    {
+        GameName.DOMoveY(TargetNamePos.position.y, 0.5f, true)
+            .OnComplete(() =>
+            {
+                Transform t = GameName.GetChild(0);
+                t.SetParent(transform);
+                t.DOPunchRotation(Vector3.forward, 1.5f);
+            });
+    }
 
     public void StartGameButton()
     {
@@ -19,13 +32,14 @@ public class MainMenu : MonoBehaviour
 
     public void CreditsButton()
     {
-        CreditsPanel.gameObject.SetActive(true);
-        MainMenuPanel.gameObject.SetActive(false);
+//        CreditsPanel.gameObject.SetActive(true);
+//        MainMenuPanel.gameObject.SetActive(false);
+
+        MainMenuPanel.DOScale(Vector3.zero, 0.5f).OnComplete(() => CreditsPanel.DOScale(Vector3.one, 0.5f));
     }
 
     public void BackButton()
     {
-        CreditsPanel.gameObject.SetActive(false);
-        MainMenuPanel.gameObject.SetActive(true);
+        CreditsPanel.DOScale(Vector3.zero, 0.5f).OnComplete(() => MainMenuPanel.DOScale(Vector3.one, 0.5f));
     }
 }

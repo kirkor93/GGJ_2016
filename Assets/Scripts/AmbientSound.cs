e450740876c0d087da2cbfda35941539c6a87a43
wisniaCFC <adam.wisniewski93@gmail.com>
2016-01-31 13:10:41 +0100

    dźwięki C.D.

---
+++ Assets/Scripts/AmbientSound.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class AmbientSound : MonoBehaviour
+{
+	void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+		GetComponent<AudioSource>().volume = 0;
+		GetComponent<AudioSource>().DOFade(1, 2);
+	}
+
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+	
+	}
+
+	void OnLevelWasLoaded(int level)
+	{
+		GetComponent<AudioSource>().volume = 0;
+		GetComponent<AudioSource>().DOFade(1, 2);
+	}
+}

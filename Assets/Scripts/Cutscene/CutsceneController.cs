b018fb4a64ac15f72a1e4146a8b0a8d3952a3ce0
Krzysztof Szczech <krzysztof.szczech@hotmail.com>
2016-01-31 01:27:52 +0100

    Przygotowanie pod cutscenkę, małe zmiany w kamerach

---
+++ Assets/Scripts/Cutscene/CutsceneController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutsceneController : MonoBehaviour
+{
+    public float ReferenceScreenAspect = 16.0f/9.0f;
+    public Renderer SilverScreen;
+
+    protected void Awake()
+    {
+        Camera c = GetComponent<Camera>();
+        c.orthographicSize *= ReferenceScreenAspect/c.aspect;
+    }
+
+    private IEnumerator SwapSceneOnFinish()
+    {
+        MovieTexture texture = SilverScreen.material.mainTexture as MovieTexture;
+        if (texture != null)
+        {
+            texture.Play();
+
+            yield return new WaitForSeconds(texture.duration + 0.1f);
+        }
+        else
+        {
+            Debug.LogError("Texture assigned to silver screen is not");
+        }
+    }
+}
